6cf361f1b30370c650c73d29d5d3bf7e9c130bce
jdnjk <2394058778@qq.com>
2023-04-26 21:11:01 +0800

    e

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: 'Monday 文档',
+    description: '文档',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '指南',
+                link: '/guide/'
+              },
+            { 
+                text: '链接', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/dawangfangpi/docs' },
+                    { text: '官网', link: 'https://www.monday-ovo.top' }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+              title: "指南",
+              path: '/guide/',
+              collapsable: false, // 不折叠
+              children: [
+                { title: "介绍", path: "/guide/" },
+                { title: "基本配置", path: "/guide/basic-config.md" },
+              ],
+            }
+          ]
+    }
+}
