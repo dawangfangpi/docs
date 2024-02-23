505376494a812514efc96c20de0db68c348e2c72
jdnjk <2394058778@qq.com>
2024-02-23 16:46:55 +0800

    推进vuepress2.0

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
